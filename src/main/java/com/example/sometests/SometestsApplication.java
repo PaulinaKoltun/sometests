eb4dd6ef4d994e2cea73de5d4140472249656a70
Paulina <paulina.k7@poczta.fm>
2023-05-10 22:32:49 +0200

    Fourth commit

--- src/main/java/com/example/sometests/SometestsApplication.java
+++ src/main/java/com/example/sometests/SometestsApplication.java
@@ -2,6 +2,7 @@ package com.example.sometests;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jackson.JsonObjectDeserializer;
 
 @SpringBootApplication
 public class SometestsApplication {
@@ -9,6 +10,9 @@ public class SometestsApplication {
     public static void main(String[] args) {
         SpringApplication.run(SometestsApplication.class, args);
         System.out.println("Hello World");
+
+        StringBuilder stringBuilder = new StringBuilder(); // comment coment
+        stringBuilder.toString();
     }
 
 }
