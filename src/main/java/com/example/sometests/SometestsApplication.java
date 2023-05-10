d35310c3ae048a8c4edf0314a4f419df3ff36b2e
Paulina <paulina.k7@poczta.fm>
2023-05-10 21:57:25 +0200

    First commit

---
+++ src/main/java/com/example/sometests/SometestsApplication.java
@@ -0,0 +1,13 @@
+package com.example.sometests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SometestsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SometestsApplication.class, args);
+    }
+
+}
