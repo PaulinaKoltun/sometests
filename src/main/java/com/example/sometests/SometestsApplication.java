89d7831c1046fca175bd272334241e2246045b25
Paulina <paulina.k7@poczta.fm>
2023-05-10 22:14:03 +0200

    Second commit

--- src/main/java/com/example/sometests/SometestsApplication.java
+++ src/main/java/com/example/sometests/SometestsApplication.java
@@ -8,6 +8,7 @@ public class SometestsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SometestsApplication.class, args);
+        System.out.println("Hello World");
     }
 
 }
